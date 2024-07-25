da98e1e3c51483e7a8f7e3d7ba61f7c0eb232504
ldellinger <ldellinger@stowerscat.com>
2024-07-25 11:21:59 -0400

    Update ADUserAddressbyOU.ps1

--- ADUserAddressbyOU.ps1
+++ ADUserAddressbyOU.ps1
@@ -1,6 +1,27 @@
-﻿get-aduser -SearchBase "OU=East Knoxville,OU=AppStoreDisabled,DC=stowerscat,DC=com" -Filter * | set-aduser -streetaddress "6301 Old Rutledge Pike" -City "Knoxville" -state "TN" -postalcode "37924" -country "US"
-get-aduser -SearchBase "OU=Chattanooga,OU=AppStoreDisabled,DC=stowerscat,DC=com" -Filter * | set-aduser -streetaddress "4066 South Access Road" -City "Chattanooga" -state "TN" -postalcode "37406" -country "US"
-get-aduser -SearchBase "OU=Crossville,OU=AppStoreDisabled,DC=stowerscat,DC=com" -Filter * | set-aduser -streetaddress "215 Interchange Drive" -City "Crossville" -state "TN" -postalcode "38571" -country "US"
-get-aduser -SearchBase "OU=Sevierville,OU=AppStoreDisabled,DC=stowerscat,DC=com" -Filter * | set-aduser -streetaddress "1825 Veterans Boulevard" -City "Sevierville" -state "TN" -postalcode "37862" -country "US"
-get-aduser -SearchBase "OU=Kingsport,OU=AppStoreDisabled,DC=stowerscat,DC=com" -Filter * | set-aduser -streetaddress "9960 Airport Parkway" -City "Kingsport" -state "TN" -postalcode "37663" -country "US"
-get-aduser -SearchBase "OU=West Knoxville,OU=AppStoreDisabled,DC=stowerscat,DC=com" -Filter * | set-aduser -streetaddress "10644 Lexington Drive" -City "Knoxville" -state "TN" -postalcode "37932" -country "US"
+Import-Module ActiveDirectory
+
+# Define a function to update user attributes
+function Update-UserAttributes {
+    param (
+        [string]$OU,
+        [string]$StreetAddress,
+        [string]$City,
+        [string]$State,
+        [string]$PostalCode,
+        [string]$Country,
+        [string]$OfficeName
+    )
+
+    Get-ADUser -Filter * -SearchBase $OU | ForEach-Object {
+        Set-ADUser -Identity $_ -StreetAddress $StreetAddress -City $City -State $State -PostalCode $PostalCode -Country $Country -Replace @{physicalDeliveryOfficeName=$OfficeName}
+        Write-Output "Updated user: $($_.SamAccountName) in $OfficeName"
+    }
+}
+
+# Update attributes for each OU
+Update-UserAttributes -OU "OU=East Knoxville,OU=AppStoreDisabled,DC=stowerscat,DC=com" -StreetAddress "6301 Old Rutledge Pike" -City "Knoxville" -State "TN" -PostalCode "37924" -Country "US" -OfficeName "East Knoxville"
+Update-UserAttributes -OU "OU=Chattanooga,OU=AppStoreDisabled,DC=stowerscat,DC=com" -StreetAddress "4066 South Access Road" -City "Chattanooga" -State "TN" -PostalCode "37406" -Country "US" -OfficeName "Chattanooga"
+Update-UserAttributes -OU "OU=Crossville,OU=AppStoreDisabled,DC=stowerscat,DC=com" -StreetAddress "215 Interchange Drive" -City "Crossville" -State "TN" -PostalCode "38571" -Country "US" -OfficeName "Crossville"
+Update-UserAttributes -OU "OU=Sevierville,OU=AppStoreDisabled,DC=stowerscat,DC=com" -StreetAddress "1825 Veterans Boulevard" -City "Sevierville" -State "TN" -PostalCode "37862" -Country "US" -OfficeName "Sevierville"
+Update-UserAttributes -OU "OU=Kingsport,OU=AppStoreDisabled,DC=stowerscat,DC=com" -StreetAddress "9960 Airport Parkway" -City "Kingsport" -State "TN" -PostalCode "37663" -Country "US" -OfficeName "Kingsport"
+Update-UserAttributes -OU "OU=West Knoxville,OU=AppStoreDisabled,DC=stowerscat,DC=com" -StreetAddress "10644 Lexington Drive" -City "Knoxville" -State "TN" -PostalCode "37932" -Country "US" -OfficeName "West Knoxville"
